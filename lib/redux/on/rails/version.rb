ade7070c9166e0cd2cda70918502c504123e3665
Andrew Mason <andrewmcodes@protonmail.com>
2023-05-04 10:34:49 -0700

    chore: initial commit

---
+++ lib/redux/on/rails/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Redux
+  module On
+    module Rails
+      VERSION = "0.1.0"
+    end
+  end
+end
---
+++ spec/redux/on/rails_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Redux::On::Rails do
+  it "has a version number" do
+    expect(Redux::On::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
